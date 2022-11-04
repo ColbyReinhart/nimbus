5096267ba9d313e6dc817adc9539ba56f28b0bd9
ColbyReinhart <colbyreinhart@gmail.com>
2022-11-04 13:12:12 -0400

    Implemented file upload backend, need to test and develop frontend

--- nimbus-server/src/main.rs
+++ nimbus-server/src/main.rs
@@ -9,7 +9,9 @@ use std::path::{Path, PathBuf};
 use std::fs::read_dir;
 use std::vec::Vec;
 
-use rocket::fs::NamedFile;
+use rocket::fs::{TempFile, NamedFile};
+use rocket::form::Form;
+use rocket::http::Status;
 use rocket_dyn_templates::Template;
 
 use nimbus_server::ResourceLink;
@@ -25,7 +27,8 @@ fn rocket() -> _
 		.mount("/", routes![
 			homepage,
 			get_static,
-			get_cloud_resource
+			get_cloud_resource,
+			put_cloud_resource
 		])
 		.attach(Template::fairing())
 }
@@ -65,5 +68,53 @@ async fn get_cloud_resource(user: &str, filepath: PathBuf) -> Template
 		Template::render("view-file", context! [
 			file: std::fs::read_to_string(resource_path).unwrap()
 		])
+	}
+}
+
+// Structure for getting the uploaded file
+#[derive(FromForm)]
+pub struct FileUpload<'r>
+{
+	is_directory: bool,
+	file: TempFile<'r>
+}
+
+impl<'r> FileUpload<'r> {
+	// Move the file out of the struct
+	pub fn take_file(self) -> TempFile<'r>
+	{
+		self.file
+	}
+}
+
+// Upload user resource
+#[post("/user-files/<filepath..>", data = "<form>")]
+async fn put_cloud_resource(filepath: PathBuf, form: Form<FileUpload<'_>>) -> Status
+{
+	// Get the full path for the file based on the configured file root
+	let full_path = Path::new("user-files/").join(filepath);
+
+	// If it's a directory, create it
+	if form.is_directory
+	{
+		match std::fs::create_dir(full_path)
+		{
+			Ok(()) => { return Status::Created },
+			Err(_) => { return Status::InternalServerError }
+		}
+	}
+	// Otherwise, create the file
+	else
+	{
+		// Try to get the uploaded file and save it to full_path, return 500 otherwise
+		match form.into_inner().take_file().persist_to(full_path).await
+		{
+			Ok(()) => {
+				Status::Created
+			},
+			Err(_) => {
+				Status::InternalServerError
+			}
+		}
 	}
 }
