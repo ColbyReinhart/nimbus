e032c272be1fe218a609273aaff885660aca57e8
Colby Reinhart <colbyreinhart@gmail.com>
2023-01-02 19:53:40 -0500

    Working on adding file input modal

--- nimbus-server/static/js/file-explorer.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#formSubmit").addEventListener("click", () => {
-	console.log("test");
-	let uploadForm = document.querySelector("#uploadResource");
-	uploadForm.action = document.querySelector("#uploadFile").files[0].name;
-	console.log(uploadForm.action);
-	uploadForm.submit();
-});
